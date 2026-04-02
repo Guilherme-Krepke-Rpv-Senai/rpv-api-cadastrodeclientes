23062cc922651616fbb01377e8807f11b8d8c678
danielsilvabm <daniel.silvabmartins@gmail.com>
2026-04-01 21:12:17 -0300

    feat: commit para atualizar

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,13 +1,28 @@
 import { FormType } from "@/pages/clientes/criar"
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form"
 
+const sizeClassMap = {
+    1: 'md:col-span-1',
+    2: 'md:col-span-2',
+    3: 'md:col-span-3',
+    4: 'md:col-span-4',
+    5: 'md:col-span-5',
+    6: 'md:col-span-6',
+    7: 'md:col-span-7',
+    8: 'md:col-span-8',
+    9: 'md:col-span-9',
+    10: 'md:col-span-10',
+    11: 'md:col-span-11',
+    12: 'md:col-span-12'
+} as const
+
 type TInput<T extends FieldValues> = {
     required: boolean
     label: string
     name: keyof T
     register: UseFormRegister<T>
     errors: FieldErrors<T>
-    size: number
+    size: keyof typeof sizeClassMap
 }
 
 export function Input({
@@ -18,10 +33,8 @@ export function Input({
     required,
     size
 }: TInput<FormType>) {
-    console.log('eerors component', name)
-
     return(
-        <div className={`relative col-span-${size} flex flex-col my-5`}>
+        <div className={`col-span-12 sm:col-span-6 ${sizeClassMap[size]} relative flex flex-col`}>
             <label>{label}{required && (<span className='text-red-500'>*</span>)}: </label>
             <input {...register(name)} className='border rounded-md px-2 py-1 text-zinc-100'/>
             <span className='absolute top-16 text-xs text-red-500'>{errors[name]?.message}</span>
